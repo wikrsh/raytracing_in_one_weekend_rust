9df5537fa71172fa5253b9584f1014a7a4e06e0a
Wataru Ikarashi <wikrsh@gmail.com>
2021-04-13 01:12:32 +0900

    Chapter 13

--- src/main.rs
+++ src/main.rs
@@ -26,63 +26,95 @@ fn ray_color<T: Hittable>(r: &Ray, world: &T, depth: i32) -> Color {
     (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
 }
 
+fn random_scene() -> HittableList {
+    let mut world = HittableList::new();
+
+    let ground_material: Rc<Box<dyn Material>> =
+        Rc::new(Box::new(Lambertian::new(Color::new(0.5, 0.5, 0.5))));
+    world.add(Box::new(Sphere::new(
+        Vec3::new(0.0, -1000.0, 0.0),
+        1000.0,
+        &ground_material,
+    )));
+
+    for a in -11..11 {
+        for b in -11..11 {
+            let choose_mat: f64 = random();
+            let center = Vec3::new(
+                (a as f64) + 0.9 * random::<f64>(),
+                0.2,
+                (b as f64) + 0.9 * random::<f64>(),
+            );
+
+            let sphere_material: Rc<Box<dyn Material>> = if choose_mat < 0.8 {
+                // diffuse
+                let albedo = Color::new_random(0.0, 1.0) * Color::new_random(0.0, 1.0);
+                Rc::new(Box::new(Lambertian::new(albedo)))
+            } else if choose_mat < 0.95 {
+                // metal
+                let albedo = Color::new_random(0.5, 1.0);
+                let fuzz = 0.5 * random::<f64>();
+                Rc::new(Box::new(Metal::new(albedo, fuzz)))
+            } else {
+                // grass
+                Rc::new(Box::new(Dielectric::new(1.5)))
+            };
+
+            world.add(Box::new(Sphere::new(center, 0.2, &sphere_material)));
+        }
+    }
+
+    let material1: Rc<Box<dyn Material>> = Rc::new(Box::new(Dielectric::new(1.5)));
+    world.add(Box::new(Sphere::new(
+        Vec3::new(0.0, 1.0, 0.0),
+        1.0,
+        &material1,
+    )));
+
+    let material2: Rc<Box<dyn Material>> =
+        Rc::new(Box::new(Lambertian::new(Color::new(0.4, 0.2, 0.1))));
+    world.add(Box::new(Sphere::new(
+        Vec3::new(-4.0, 1.0, 0.0),
+        1.0,
+        &material2,
+    )));
+
+    let material3: Rc<Box<dyn Material>> =
+        Rc::new(Box::new(Metal::new(Color::new(0.7, 0.6, 0.5), 0.0)));
+    world.add(Box::new(Sphere::new(
+        Vec3::new(4.0, 1.0, 0.0),
+        1.0,
+        &material3,
+    )));
+
+    world
+}
+
 fn main() -> io::Result<()> {
     // Image
-    let aspect_ratio = 16.0 / 9.0;
-    let image_width: usize = 400;
+    let aspect_ratio = 3.0 / 2.0;
+    let image_width: usize = 1200;
     let image_height: usize = (image_width as f64 / aspect_ratio) as usize;
-    let samples_per_pixel = 100;
+    let samples_per_pixel = 500;
     let max_depth = 50;
 
     // World
-
-    let mut world = HittableList::new();
-
-    let material_ground: Rc<Box<dyn Material>> =
-        Rc::new(Box::new(Lambertian::new(Color::new(0.8, 0.8, 0.0))));
-    let material_center: Rc<Box<dyn Material>> =
-        Rc::new(Box::new(Lambertian::new(Color::new(0.1, 0.2, 0.5))));
-    let material_left: Rc<Box<dyn Material>> = Rc::new(Box::new(Dielectric::new(1.5)));
-    let material_right: Rc<Box<dyn Material>> =
-        Rc::new(Box::new(Metal::new(Color::new(0.8, 0.6, 0.2), 0.0)));
-
-    world.add(Box::new(Sphere::new(
-        Vec3::new(0.0, -100.5, -1.0),
-        100.0,
-        &material_ground,
-    )));
-    world.add(Box::new(Sphere::new(
-        Vec3::new(0.0, 0.0, -1.0),
-        0.5,
-        &material_center,
-    )));
-    world.add(Box::new(Sphere::new(
-        Vec3::new(-1.0, 0.0, -1.0),
-        0.5,
-        &material_left,
-    )));
-    world.add(Box::new(Sphere::new(
-        Vec3::new(-1.0, 0.0, -1.0),
-        -0.45,
-        &material_left,
-    )));
-    world.add(Box::new(Sphere::new(
-        Vec3::new(1.0, 0.0, -1.0),
-        0.5,
-        &material_right,
-    )));
+    let world = random_scene();
 
     // Camera
-    let lookfrom = Vec3::new(3.0, 3.0, 2.0);
-    let lookat = Vec3::new(0.0, 0.0, -1.0);
+    let lookfrom = Vec3::new(13.0, 2.0, 3.0);
+    let lookat = Vec3::new(0.0, 0.0, 0.0);
+    let dist_to_focus = 10.0;
+    let aperture = 0.1;
+
     let camera = Camera::new(
         lookfrom,
         lookat,
         Vec3::new(0.0, 1.0, 0.0),
         20.0,
         aspect_ratio,
-        2.0,
-        (lookfrom - lookat).length(),
+        aperture,
+        dist_to_focus,
     );
 
     // Render
